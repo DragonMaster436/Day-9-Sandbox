c0fb24ce68eb2fe62be0e0d451b72aaa8cbf7cae
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 20:40:06 -0600

    Add files via upload

---
+++ Day-9-Sandbox.py
@@ -0,0 +1,39 @@
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.", 
+#     "Function": "A piece of code that you can easily call over and over again.",
+# }
+
+
+# new_dictionary = {}
+
+
+# print(programming_dictionary)
+# print(programming_dictionary["Bug"])
+# new_dictionary["apples"] = "they taste good."
+# print(new_dictionary["apples"])
+
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary)
+#     print(programming_dictionary[key])
+
+# for key in programming_dictionary:
+#     new_dictionary[key] = programming_dictionary[key]
+
+# print(new_dictionary)
+
+
+# travel_log = {
+#     "France": {
+#         "cities_visited": ["Place1", "Place2", "Place3"] "total_visits": 12,
+#     }
+#     "Germany": ["thing1", "thing2", "thing3",]
+# }
+
+# travel_log = {
+#     "France": {
+#         "cities_visited": ["Place1", "Place2", "Place3"] "total_visits": 12,
+#     }
+#     "Germany": ["thing1", "thing2", "thing3",]
+# }
+
